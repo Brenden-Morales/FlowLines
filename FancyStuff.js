589d718ab7d4449c6d972007817e0411ec18330f
Brenden Morales <bmorales@SPWRK085.WindLogics.local>
2015-11-23 12:53:41 -0600

    Main render loop done

--- FancyStuff.js
+++ FancyStuff.js
@@ -3,7 +3,12 @@
  */
 var threeInitialized = false;
 
-var renderer;
+var renderer,stats,scene,camera;
+
+var continueRender = false;
+
+var totalTime = 0;
+var startTime = Date.now();
 
 function startRendering(){
     console.log("start Rendering");
@@ -19,11 +24,48 @@ function startRendering(){
         renderer.setClearColor(0xffffff,0);
         renderer.setSize( window.innerWidth, window.innerHeight );
 
+        //setup camera
+        camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 1, 20);
+        camera.position.z = 10;
+
+        //setup scene
+        scene = new THREE.Scene();
+
+        //setup stats
+        stats = new Stats();
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.top = '0px';
+        stats.domElement.style.zIndex = 100;
+        document.getElementById("overlay").appendChild( stats.domElement );
+
+        var plane = new THREE.Mesh(new THREE.PlaneGeometry(window.innerWidth,window.innerHeight,1,1),new THREE.MeshBasicMaterial({color:0xff0000}));
+        scene.add(plane);
 
         threeInitialized = true;
     }
+
+    continueRender = true;
+    animate();
+
 }
 
 function stopRendering(){
-    console.log("stop rendering");
+    continueRender = false;
+}
+
+
+//we just keep looping through this function forever
+function animate(){
+    if(!continueRender)
+        return;
+    requestAnimationFrame(animate);
+    render();
+}
+
+//the actual render function
+function render(){
+    var delta = Date.now() - startTime;
+    totalTime += delta;
+    startTime = Date.now();
+    renderer.render(scene,camera)
 }
