c11b9830a9982c5e11d1e206dcda58bba20e6ea8
Brenden Morales <bmorales@SPWRK085.WindLogics.local>
2015-12-04 09:24:20 -0600

    Debug plane?

---
+++ DebugPlane.js
@@ -0,0 +1,10 @@
+/**
+ * Created by bmorales on 12/3/2015.
+ */
+var DebugPlane = function(options){
+    var self = this instanceof DebugPlane ? this : Object.create(DebugPlane.prototype);
+
+
+
+    return self;
+};
